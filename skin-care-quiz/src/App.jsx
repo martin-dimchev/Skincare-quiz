72ecb3a59edff8e31b0092d76e09928b90ba71ba
martin-dimchev <martin.dimchev@abv.bg>
2025-07-03 00:21:36 +0300

    add: Added home page

---
+++ skin-care-quiz/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="background">
+      <div className='overlay'>
+      <div className="main-content">
+        <h1>Build a self care routine<br />suitable for you</h1>
+        <p>Take out test to get a personalised self care<br />routine based on your needs.</p>
+        <button className="quiz-button">Start the quiz</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App
